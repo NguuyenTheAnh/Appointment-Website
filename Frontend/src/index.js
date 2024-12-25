80821c1fafb7d60a30d42e71ac03155084a2923e
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2024-12-25 21:17:43 +0700

    use redux-perist

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'nprogress/nprogress.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import RouteApp from './RouteApp';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <RouteApp />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <RouteApp />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
